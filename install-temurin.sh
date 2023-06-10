b1fd2c0794d7c77b2b6fea8bd4d9994c6a08b8a0
Guilherme Ribeiro <guilhermelionribeiro@gmail.com>
2023-06-10 16:38:14 -0300

    adds script to install temurin

---
+++ install-temurin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install -y software-properties-common wget apt-transport-https
+sudo mkdir -p /etc/apt/keyrings
+wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
+sudo apt-get update
+sudo apt-get install -y temurin-17-jdk
